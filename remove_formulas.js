5ee988792c9d9ba44a539050a499edc540fb5dc5
alxhfd <127575917+alxhfd@users.noreply.github.com>
2023-09-16 15:12:42 +0100

    Update remove_formulas.js

--- remove_formulas.js
+++ remove_formulas.js
@@ -1,13 +1,10 @@
 function main(workbook: ExcelScript.Workbook) {
   // copy and paste as values entire excel sheets
   let sheetNames = ["Sheet1", "Sheet2", "Sheet3"];
-  
   for (let i = 0; i < sheetNames.length; i++) {
-    let sheetName = sheetNames[i];
-    let sheet = workbook.getWorksheet(sheetName);
+    let sheet = workbook.getWorksheet(sheetNames[i]);
     let rangeToCopy = sheet.getUsedRange();
     rangeToCopy.copyFrom(rangeToCopy, ExcelScript.RangeCopyType.values, false);
   }
-
   workbook.save();
 }
